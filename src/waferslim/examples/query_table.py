fb2ebb0d8d28c2885deaa18346c17a954f4008a9
tim <tim@lightning>
2009-03-05 19:41:11 +0000

    Added example of custom converter with query table

--- src/waferslim/examples/query_table.py
+++ src/waferslim/examples/query_table.py
@@ -1,10 +1,39 @@
-''' Example of a Slim QueryTable -- 
+''' Example of a Slim QueryTable and a custom Converter -- 
 based on http://fitnesse.org/FitNesse.SliM.QueryTable'''
 
-from waferslim.converters import convert_arg
+from waferslim.converters import convert_arg, Converter, convert_value, \
+    register_converter
 import datetime
 
-class EmployeesHiredBefore(object):
+class Employee:
+    ''' Simple example employee class with data but no behaviour (!) '''
+    def __init__(self, emp_no, fname, lname, hiredate_tuple):
+        self._emp_no = emp_no
+        self._fname = fname
+        self._lname = lname
+        self._hired = datetime.date(*hiredate_tuple)
+        
+    def as_dict(self):
+        ''' Extract the employee's data as a dict '''
+        return {'employee number':self._emp_no,
+                'first name':self._fname,
+                'last name':self._lname,
+                'hire date':self._hired
+                }
+        
+class EmployeeConverter(Converter):
+    ''' Custom converter for Employee instances. Because this is being used
+    in a query table we need to ensure that each Employee is converted to a 
+    list made up of [name, value] pairs. Note the final convert_value() call
+    to ensure that the contents of all the returned lists are str-converted '''
+    def to_string(self, employee):
+        dict_items = employee.as_dict().items
+        return convert_value( [[key, value] for key, value in dict_items()] )
+
+# Don't forget to register the custom converter!
+register_converter(Employee, EmployeeConverter())
+
+class EmployeesHiredBefore:
     ''' Class to be the system-under-test in fitnesse. '''
 
     @convert_arg(to_type=datetime.date)
@@ -15,24 +44,12 @@ class EmployeesHiredBefore(object):
         
     def query(self):
         ''' Standard slim method for query tables. Returns a list of 
-        lists of lists: each "row" of the query result is a list that contains
-        a field-value list for each field value''' 
+        values: each "row" of the query result is an element in the list.''' 
         return self._simulate_query(self._before_date)
     
     def _simulate_query(self, for_date_parameter):
-        ''' Simulate performing a query e.g. on a database ''' 
+        ''' Simulate performing a query e.g. on a database '''
         return [
-                [
-                ['employee number', '1429'],
-                ['first name', 'Bob'],
-                ['last name', 'Martin'],
-                ['hire date', '1974-10-10'],
-                ],
-                [
-                ['employee number', '8832'],
-                ['first name', 'James'],
-                ['last name', 'Grenning'],
-                ['hire date', '1979-12-15'],
-                ]
+                Employee(1429, 'Bob', 'Martin', (1974, 10, 10)),
+                Employee(8832, 'James', 'Grenning', (1979, 12, 15))
                ]
-#TODO: Use People objects with conversion. List of str requires conversion with " not ' ! :-()
