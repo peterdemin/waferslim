81b2fd477696d7d444601c1e55a65378238df2c4
tim <tim@lightning>
2009-03-05 15:05:55 +0000

    Tidied up decision table example. Added script table example

--- src/waferslim/examples/decision_table.py
+++ src/waferslim/examples/decision_table.py
@@ -1,7 +1,12 @@
-''' Example of a DecisionTable -- 
-taken from http://fitnesse.org/FitNesse.SliM.DecisionTable'''
+''' Example of a Slim DecisionTable -- 
+based on http://fitnesse.org/FitNesse.SliM.DecisionTable'''
 
-from waferslim.converters import convert_arg
+from waferslim.converters import convert_arg, register_converter, \
+                                 YesNoConverter
+
+# Most conversion can be handled using the standard registered converters
+# but we've chosen to use bool / YesNoConverter for table readability.
+register_converter(bool, YesNoConverter())
 
 class ShouldIBuyMilk:
     ''' Class to be the system-under-test in fitnesse. '''
@@ -15,25 +20,30 @@ class ShouldIBuyMilk:
     @convert_arg(to_type=int)
     def setCashInWallet(self, int_amount):
         ''' Decorated method to set cash as an int.
-        The decorator translates from a standard slim string value. '''
+        The decorator uses the implicitly registered int converter to 
+        translate from a standard slim string value to an int. '''
         self._cash = int_amount
         
     @convert_arg(to_type=bool)
     def setCreditCard(self, bool_value):
         ''' Decorated method to set credit as a bool.
-        The decorator translates from a standard slim string value. '''
+        The decorator uses the explicitly registered bool converter to 
+        translate from a standard slim string value. '''
         self._credit = bool_value
         
     @convert_arg(to_type=int)
     def setPintsOfMilkRemaining(self, int_amount):
         ''' Decorated method to set pints of milk as an int.
-        The decorator translates from a standard slim string value. '''
+        The decorator uses the implicitly registered int converter to 
+        translate from a standard slim string value to an int. '''
         self._pints = int_amount
-        
+    
     def goToStore(self):
         ''' Return whether I should go to the store or not.
-        The result will be translated to a standard slim string value
-        without needing decoration, when invoked through waferslim. '''
+        The bool return value will be converted to a str with the
+        YesNoConverter we explicitly registered. For examples of
+        methods with return values that are not decorated,
+        because the conversion is done implicitly, see script_table).'''
         return self._pints == 0 and (self._credit or self._cash > 2)
 
 class ShouldIBuyMilkAlternativeImplementation(ShouldIBuyMilk):
@@ -52,11 +62,11 @@ class ShouldIBuyMilkAlternativeImplementation(ShouldIBuyMilk):
     
     def execute(self):
         ''' Slim-standard method that will be invoked after variables are set,
-        if it exists ''' 
+        if it is present ''' 
         if self._pints == 0 and (self._credit or self._cash > 2):
             self._goToStore = True
     
     def reset(self):
         ''' Slim-standard method that will be invoked before each table row, 
-        if it exists ''' 
+        if it is present ''' 
         self._goToStore = False
---
+++ src/waferslim/examples/script_table.py
@@ -0,0 +1,45 @@
+''' Example of a Slim ScriptTable -- 
+based on http://fitnesse.org/FitNesse.SliM.ScriptTable'''
+
+from waferslim.converters import convert_arg, register_converter, \
+                                 TrueFalseConverter
+
+# At present, script tables require TrueFalseConverter 
+register_converter(bool, TrueFalseConverter())
+
+class LoginDialogDriver:
+    ''' Class to be the system-under-test in fitnesse. '''
+    
+    def __init__(self, user_name, password):
+        ''' New instance for the user_name and password specified has 0
+        current login attempts and no message '''
+        self._user_name = user_name
+        self._password = password
+        self._login_attempts = 0
+        self._message = None
+        
+    def loginWithUsernameAndPassword(self, user_name, password):
+        ''' Attempt to login with a user_name/ password combination. Fails
+        unless args specified here match those specified in __init__.
+        Note: no conversion-related decoration of this method is required 
+        because the params are str-s and the return bool value is implicitly
+        converted using the default TrueFalseConverter'''
+        self._login_attempts += 1
+        if self._user_name == user_name and self._password == password:
+            self._message = '%s logged in.' % user_name
+            return True
+        self._message = '%s not logged in.' % user_name
+        return False
+    
+    def loginMessage(self):
+        ''' Expose the internals of the sut to check any generated message.
+        Note: no conversion-related decoration of this method is required 
+        because there are no params and the return value is already a str'''
+        return self._message
+
+    def numberOfLoginAttempts(self):
+        ''' Expose the internals of the sut to check any generated message.
+        Note: no conversion-related decoration of this method is required 
+        because there are no params and the numeric return value is 
+        implicitly converted by waferslim'''
+        return self._login_attempts 
