a7ad0c8711882d6768ea8f5caf34c19448ab6f23
tim <tim@lightning>
2010-03-28 10:47:56 -0400

    Upped version number

--- src/waferslim/__init__.py
+++ src/waferslim/__init__.py
@@ -7,7 +7,7 @@ The latest source code is available at http://code.launchpad.net/waferslim.
 Copyright 2009-2010 by the author(s). All rights reserved 
 '''
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 class WaferSlimException(Exception):
     ''' Base Exception class for this package '''
